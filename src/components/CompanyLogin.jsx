b180074137d4f69669a0c1fa90b2b7ad9caef822
khaled-el-badawy <khaled1kkhaled@gmail.com>
2026-02-27 20:45:50 +0200

    تعديل تصميم شاشة تسجيل الدخول: إعادة هيكلة العناصر وتحسين التنسيق لزيادة التفاعل وسهولة الاستخدام company.jsx company.css

--- src/components/CompanyLogin.jsx
+++ src/components/CompanyLogin.jsx
@@ -55,104 +55,103 @@ function CompanyLogin() {
   return (
     <div className="company-login-container">
       <motion.div
-        className="Login-form"
-        initial={{ y: 60, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
-      >
-        <h1 className="title">اهلاً بعودتك</h1>
-
-        <div className="login-form-fields">
-          <form onSubmit={(e) => e.preventDefault()}>
-            {/* حقل اسم المستخدم */}
-            <div className="field-container">
-              <input
-                type="text"
-                placeholder="اسم الشركة"
-                value={username}
-                onChange={handleUsernameChange}
-                required
-              />
-              {usernameError && (
-                <p className="login-error-msg">{usernameError}</p>
-              )}
-            </div>
-
-            {/* حقل كلمة السر */}
-            <div className="field-container login-password-field">
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="كلمة السر"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <span
-                className="password-eye"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <FiEye /> : <FiEyeOff />}
-              </span>
-              {passwordError && (
-                <p className="login-error-msg">{passwordError}</p>
-              )}
-            </div>
-
-            <h3>
-              <Link to="#" className="h3">
-                هل نسيت كلمة السر؟
-              </Link>
-            </h3>
-
-            {/* زر تسجيل الدخول */}
-            <Link to="/home">
-              <button
-                type="button"
-                className="btn-container"
-                onClick={handleLogin}
-                disabled={!isFormValid}
-                style={{
-                  pointerEvents: !isFormValid ? "none" : "auto",
-                  opacity: !isFormValid ? 0.5 : 1,
+              className="Login-form"
+              initial={{ y: 60, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
+            >
+              <div className="login-form-fields">
+                <form onSubmit={(e) => e.preventDefault()}>
+                  <h1 className="title">اهلاً بعودتك</h1>
+                  {/* حقل اسم المستخدم */}
+                  <div className="field-container">
+                    <input
+                      type="text"
+                      placeholder="اسم المستخدم"
+                      value={username}
+                      onChange={handleUsernameChange}
+                      required
+                    />
+                    {usernameError && (
+                      <p className="login-error-msg">{usernameError}</p>
+                    )}
+                  </div>
+      
+                  {/* حقل كلمة السر */}
+                  <div className="field-container login-password-field">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="كلمة السر"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <span
+                      className="password-eye"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FiEye /> : <FiEyeOff />}
+                    </span>
+                    {passwordError && (
+                      <p className="login-error-msg">{passwordError}</p>
+                    )}
+                  </div>
+      
+                  <h3>
+                    <Link to="#" className="forgot-password-link">
+                      هل نسيت كلمة السر؟
+                    </Link>
+                  </h3>
+      
+                  {/* زر تسجيل الدخول */}
+                  <Link to="/home">
+                    <button
+                      type="button"
+                      className="btn-container"
+                      onClick={handleLogin}
+                      disabled={!isFormValid}
+                      style={{
+                        pointerEvents: !isFormValid ? "none" : "auto",
+                        opacity: !isFormValid ? 0.5 : 1,
+                      }}
+                    >
+                      تسجيل الدخول
+                    </button>
+                  </Link>
+                  <h4 className="register-link">
+                    ليس لديك حساب ؟{" "}
+                    <Link to="/CustomerRegister" className="link">
+                      إنشاء حساب
+                    </Link>
+                  </h4>
+                </form>
+              </div>
+            </motion.div>
+            {/* -------------------------------------------------------------- */}
+            <div className="image">
+              <motion.img
+                src="/images/Frame 20.svg"
+                initial={{ x: "20%", y: 0, opacity: 0 }}
+                animate={{
+                  x: 0,
+                  y: [0, -10, 0],
+                  opacity: 1,
                 }}
-              >
-                تسجيل الدخول
-              </button>
-            </Link>
-            <h4 className="h4">
-              ليس لديك حساب ؟{" "}
-              <Link to="/CompanyRegister" className="link">
-                إنشاء حساب
-              </Link>
-            </h4>
-          </form>
-        </div>
-      </motion.div>
-
-      <div className="image">
-        <motion.img
-          src="/images/Frame 20.svg"
-          initial={{ x: "20%", y: 0, opacity: 0 }}
-          animate={{
-            x: 0,
-            y: [0, -10, 0],
-            opacity: 1,
-          }}
-          transition={{
-            x: { duration: 1.8, ease: "easeOut" },
-            y: { duration: 3, ease: "easeInOut", repeat: Infinity },
-            opacity: { duration: 1.8, ease: "easeOut" },
-          }}
-          style={{
-            height: "100%",
-            objectFit: "cover",
-            display: "block",
-            flexShrink: 0,
-            position: "relative",
-            zIndex: 1,
-          }}
-        />
-      </div>
-    </div>
+                transition={{
+                  x: { duration: 1.8, ease: "easeOut" },
+                  y: { duration: 3, ease: "easeInOut", repeat: Infinity },
+                  opacity: { duration: 1.8, ease: "easeOut" },
+                }}
+                style={{
+                  height: "100%",
+                  objectFit: "cover",
+                  display: "block",
+                  flexShrink: 0,
+                  position: "relative",
+                  zIndex: 1,
+                }}
+              />
+            </div>
+          </div>
   );
 }
 
