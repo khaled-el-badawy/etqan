23eda890745d6eee1c01671de4811dc30673e0c3
khaled-el-badawy <khaled1kkhaled@gmail.com>
2026-04-05 00:44:34 +0200

    Refactor Vite configuration to use mode parameter for base path determination

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.MODE === "production" ? "/etqan/" : "/",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/etqan/" : "/",
+  };
 });
