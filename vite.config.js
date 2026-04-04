3dbe6da76e9c94e8b6aac4e9dfc77d9be0d7be59
khaled-el-badawy <khaled1kkhaled@gmail.com>
2026-04-05 00:22:48 +0200

    Add React plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@
 
 ///////////////////by khaled
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   base: "/etqan/",
 });
